8c84f545deac4fa30e8744cffbf0d8d51af8ce0b
Miles Gerrit Cochran Branson <mcochran@lxplus784.cern.ch>
2022-07-07 01:35:02 +0200

    edits

--- taunet/models.py
+++ taunet/models.py
@@ -32,24 +32,16 @@ def keras_model_terry(n_variables, name='more_simple_dnn'):
     output   = tf.keras.layers.Dense(1, activation='linear')(hidden_4)
     return tf.keras.Model(inputs=x_1, outputs=output, name=name)
 
-def keras_model_simple(n_variables, name='most_simple_dnn'):
-    x_1 = tf.keras.Input(shape=n_variables)
-    hidden_0 = tf.keras.layers.Dense(128, activation='relu')(x_1)
-    hidden_1 = tf.keras.layers.Dense(64, activation='relu')(hidden_0)
-    output   = tf.keras.layers.Dense(1, activation='linear')(hidden_1)
-    return tf.keras.Model(inputs=x_1, outputs=output, name=name)
-
 def keras_model_mdn(n_variables, name='simple_mdn'):
     x_1 = tf.keras.Input(shape=n_variables)
     # create some densely-connected NN layers
     # relu = rectified linear unit activation, i.e. f(x) = max(x, 0)
-    hidden_0 = tf.keras.layers.Dense(1024, activation='relu')(x_1)
-    hidden_1 = tf.keras.layers.Dense(512, activation='relu')(hidden_0)
-    hidden_2 = tf.keras.layers.Dense(256, activation='relu')(hidden_1)
+    hidden_0 = tf.keras.layers.Dense(192, activation='relu')(x_1)
+    hidden_1 = tf.keras.layers.Dense(192, activation='relu')(hidden_0)
+    hidden_2 = tf.keras.layers.Dense(192, activation='relu')(hidden_1)
     hidden_3 = tf.keras.layers.Dense(128, activation='relu')(hidden_2)
     hidden_4 = tf.keras.layers.Dense(64, activation='relu')(hidden_3)
-    hidden_5 = tf.keras.layers.Dense(32, activation='relu')(hidden_4)
-    hidden_6 = tf.keras.layers.Dense(16, activation='relu')(hidden_5)
-    hidden_7 = tf.keras.layers.Dense(8, activation='relu')(hidden_6)
-    output = tfp.layers.MixtureNormal(1)(hidden_7)
+    output   = tfp.layers.MixtureNormal(1)(hidden_4)
     return tf.keras.Model(inputs=x_1, output=output, name=name)
+
+test = 32;
