edits to fix normalization!

taunet/database.py
@@ -164,9 +164,10 @@ def testing_data(
                 select_3p=select_3p)
             f = np.stack(
                 [ak.flatten(a[__feat]).to_numpy() for __feat in features])
+            print('Shape of f is {}'.format(np.shape(f)))
             # Optionally normalize data if done in the training
             if not no_normalize:
-                f = applySSNormalizeTest(f, norms)
+                f = applySSNormalizeTest(f, norms, vars=getVarIndices(features, VARNORM))
                 log.info('Normalizing input data to regressor')
             regressed_target = regressor.predict(f.T)
             if not no_norm_target: