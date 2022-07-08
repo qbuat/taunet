edits to fix normalization things

taunet/computation.py
@@ -1,10 +1,12 @@
 import numpy as np
+from numpy import empty
 
 from taunet.fields import FEATURES
 
 from . import log; log = log.getChild(__name__)
 
-#%% Just a function for computing chi^2
+#%%-------------------------------------------------------------
+# Implement chi^2 test
 def chi_squared(obs, exp):
     """
     Compute chi squared of variable obs wrt exp (expectation)
@@ -13,11 +15,11 @@ def chi_squared(obs, exp):
     for i in range(len(obs)):
         if exp[i] != 0:
             chi_squared += (obs[i] - exp[i]) ** 2 / exp[i]
-        else:
-            log.info('Potential error: expected value was zero!')
     return chi_squared
 
-#%% Functions for scaling variables
+#%%-------------------------------------------------------------
+# Normalization functions
+
 def StandardScalar(x, mean, std):
     """
     Standard Scalar function for pre-processing data 
@@ -28,11 +30,9 @@ def StandardScalar(x, mean, std):
     else:
         return (x - mean) / std
 
-def getSSNormalize(data, target):
+def getSSNormalize(data, target, savepath='data/normFactors'):
     """
     Pre-process data using the standard scalar function. 
-    Optionally select which variables to scale using the vars argument. 
-    Takes a vector 
     """
     norms = []
     for i in range(len(data[1,:])):
@@ -43,11 +43,14 @@ def getSSNormalize(data, target):
     mean = np.mean(target)
     std = np.std(target)
     norms.append([mean, std])
-    np.save('data/normFactors', norms)
+    np.save(file=savepath, arr=norms)
+    log.info('Saving normalization to {}.npy'.format(savepath))
     return norms
 
 def applySSNormalize(data, norms, vars=[]):
     """
+    Use already find means and std to re-shape data
+    Optionally choose which variables to normalize
     """
     if vars == []:
         vars = range(len(data[0,:]))
@@ -55,17 +58,27 @@ def applySSNormalize(data, norms, vars=[]):
         data[:,i] = StandardScalar(data[:,i], norms[i][0], norms[i][1])
     return data; 
 
-def applySSNormalizeTest(data, norms):
+def applySSNormalizeTest(data, norms, vars=[]):
     """
+    Apply norms to testing data. 
     """
-    for i in range(len(data)):
-        data = StandardScalar(data, norms[i][0], norms[i][1])
+    if vars == []:
+        vars = range(len(data[:,0]))
+    for i in vars:
+        data[i,:] = StandardScalar(data[i,:], norms[i][0], norms[i][1])
     return data; 
 
-# indices of variables to normalize
-# Note: variable 12 doesn't really need to be normalized
-# Update function getVarIndices to make this clearer... 
-NormVarIndices = [0, 1, 2, 3, 4, 5, 9, 12, len(FEATURES)-1]
+def getVarIndices(features, vars=FEATURES):
+    """
+    Get indices of variable to apply normalization to
+    """
+    i = 0
+    indices = []
+    for _feat in features:
+        if _feat in vars:
+            indices += [i]
+        i = i + 1
+    return indices
 
 # variables to normalize
 VARNORM = [
@@ -75,59 +88,14 @@ VARNORM = [
     'TauJetsAuxDyn.ClustersMeanCenterLambda',
     'TauJetsAuxDyn.ClustersMeanFirstEngDens',
     'TauJetsAuxDyn.ClustersMeanSecondLambda',
-    'TauJetsAuxDyn.ptCombined',
+    #'TauJetsAuxDyn.ptCombined',
     'TauJetsAuxDyn.etaPanTauCellBased',
     'TauJetsAuxDyn.ptTauEnergyScale'
 ]
 
-def getVarIndices(features, vars=FEATURES):
-    i = 0
-    indices = []
-    for _feat in features:
-        if _feat in vars:
-            indices += [i]
-        i = i + 1
-    return indices
-
-#%%-----------------------------------------------------------
-# Loss function for MDN
-# Function from https://gist.github.com/sergeyprokudin/4a50bf9b75e0559c1fcd2cae860b879e
-from keras import backend
-
-
-
-    # """Keras implmementation of multivariate Gaussian negative loglikelihood loss function. 
-    # This implementation implies diagonal covariance matrix.
-    
-    # Parameters
-    # ----------
-    # ytrue: tf.tensor of shape [n_samples, n_dims]
-    #     ground truth values
-    # ypreds: tf.tensor of shape [n_samples, n_dims*2]
-    #     predicted mu and logsigma values (e.g. by your neural network)
-        
-    # Returns
-    # -------
-    # neg_log_likelihood: float
-    #     negative loglikelihood averaged over samples
-        
-    # This loss can then be used as a target loss for any keras model, e.g.:
-    #     model.compile(loss=gaussian_nll, optimizer='Adam') 
-    
-    # """
-
-def gaussian_nll(y_true, y_pred, sample_weight=None):
-    mu = y_pred[:,0]
-    logsigma = y_pred[:,1]
-    
-    mse = -0.5*backend.square((y_true-mu)/backend.exp(logsigma))
-    log2pi = -0.5*np.log(2*np.pi)
-    
+#%%------------------------------------------------
+# MDN loss function
+def tf_mdn_loss(y, model, sample_weight=None):
     if sample_weight is not None:
-        print('Using Sample Weight!')
-        log_likelihood = (mse - logsigma + log2pi) * sample_weight
-        return -backend.sum(log_likelihood) / backend.sum(sample_weight)
-        
-    print('NOT Using Sample Weight!')
-    log_likelihood = mse - logsigma + log2pi
-    return -backend.mean(log_likelihood)
+        return -model.log_prob(y) * sample_weight
+    return -model.log_prob(y)

taunet/database.py
@@ -51,7 +51,7 @@ def retrieve_arrays(tree, fields, cut=None, select_1p=False, select_3p=False):
         arrays = arrays[ arrays['TauJetsAuxDyn.nTracks'] == 3 ]
     return arrays
         
-def training_data(path, dataset, features, target, nfiles=-1, select_1p=False, select_3p=False, use_cache=False, tree_name='CollectionTree', no_normalize=False, no_norm_target=False):
+def training_data(path, dataset, features, target, nfiles=-1, select_1p=False, select_3p=False, use_cache=False, tree_name='CollectionTree', no_normalize=False, no_norm_target=False, normSavePath='data/normFactors'):
     """
     """
     if use_cache:
@@ -99,19 +99,20 @@ def training_data(path, dataset, features, target, nfiles=-1, select_1p=False, s
         log.info('Total training input = {}'.format(_train.shape))
 
         #! added for testing
-        old_data = np.array(_train)
+        og_train = np.array(_train)
+        old_target = np.array(_target)
 
         #normalize here!
         if not no_normalize:
             log.info('Normalizing training data')
-            norms = getSSNormalize(_train, _target)
+            norms = getSSNormalize(_train, _target, savepath=normSavePath)
             _train = applySSNormalize(_train, norms, 
                         vars=getVarIndices(features, VARNORM))
             if not no_norm_target:
                 log.info('Normalizing validation data')
                 _target = StandardScalar(_target, norms[len(norms) - 1][0], norms[len(norms) - 1][1])
 
-        print((old_data == _train).all())
+        #print((old_data == _train).all())
         
         from sklearn.model_selection import train_test_split
         X_train, X_val, y_train, y_val = train_test_split(
@@ -119,7 +120,7 @@ def training_data(path, dataset, features, target, nfiles=-1, select_1p=False, s
         log.info('Total validation input {}'.format(len(X_val)))
 
 
-    return X_train, X_val, y_train, y_val, old_data, _train
+    return X_train, X_val, y_train, y_val#, og_train, _train, old_target, _target
 
 def testing_data(
         path, dataset, features, plotting_fields, regressor, 
@@ -141,6 +142,7 @@ def testing_data(
     # build unique list of variables to retrieve
     _fields_to_lookup = list(set(features + plotting_fields))
 
+    # load normalization from training if used
     if not no_normalize or not no_norm_target:
         if optional_path == '':
             norms = np.load('data/normFactors.npy')
@@ -162,13 +164,17 @@ def testing_data(
                 select_3p=select_3p)
             f = np.stack(
                 [ak.flatten(a[__feat]).to_numpy() for __feat in features])
-            #! I think the correct way to do this but still acting funky
+            print('Shape of f is {}'.format(np.shape(f)))
+            # Optionally normalize data if done in the training
             if not no_normalize:
-                f = applySSNormalizeTest(f, norms)
+                f = applySSNormalizeTest(f, norms, vars=getVarIndices(features, VARNORM))
                 log.info('Normalizing input data to regressor')
-            regressed_target = regressor.predict(f.T) #alpha norm with scaling
+            regressed_target = regressor.predict(f.T)
             if not no_norm_target:
-                regressed_target = norms[len(norms)-1][1] * regressed_target + norms[len(norms)-1][0] # revert to alpha
+                # If target was normalized, revert to original
+                # Last element of variable "norms" contains mean (element 0) 
+                # and std (element 1) of target. 
+                regressed_target = norms[len(norms)-1][1] * regressed_target + norms[len(norms)-1][0]
                 log.info('Returning data to orginal format for plotting')
             regressed_target = regressed_target.reshape((regressed_target.shape[0], ))
             _arr = np.stack(