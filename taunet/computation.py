9d60810bfe95819b247242fd728d1e81b3744ebb
lvb5 <mcochranbranson@gmail.com>
2022-07-14 19:37:12 +0200

    added back in norms for pt.combined

--- taunet/computation.py
+++ taunet/computation.py
@@ -95,7 +95,7 @@ VARNORM = [
     'TauJetsAuxDyn.ClustersMeanCenterLambda',
     'TauJetsAuxDyn.ClustersMeanFirstEngDens',
     'TauJetsAuxDyn.ClustersMeanSecondLambda',
-    #'TauJetsAuxDyn.ptCombined',
+    'TauJetsAuxDyn.ptCombined',
     'TauJetsAuxDyn.etaPanTauCellBased',
     'TauJetsAuxDyn.ptTauEnergyScale'
 ]
