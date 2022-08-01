c136febd380e48982deb1ddeb7d645eb24ec6c70
Miles Cochran-Branson <mcochranbranson@gmail.com>
2022-08-01 11:07:07 -0700

    add mean and stddev calculations to model

--- fit.py
+++ fit.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
         print (adam.learning_rate)
         adam.learning_rate = rate
         print (adam.learning_rate)
-        _epochs = 3
+        _epochs = 1
         regressor.compile(
             loss=tf_mdn_loss, 
             optimizer=adam, 
@@ -94,7 +94,13 @@ if __name__ == '__main__':
                     save_best_only=True)])
         # save only best run
         regressor.save(_model_file) # save results of training
-        np.save(file='data/distribution_weights', arr=regressor.get_weights()[len(regressor.get_weights())-1])
+        # get means and stdevs from model
+        temp = tf.constant([[1. for _ in range(len(FEATURES))]])
+        dist = regressor(temp)
+        means = dist.tensor_distribution.components_distribution.tensor_distribution.mean().numpy()
+        stddevs = dist.tensor_distribution.components_distribution.tensor_distribution.stddev().numpy()
+        dictionary = {'mean':means.flatten(), 'stddevs':stddevs.flatten()}
+        pickle.dump(dictionary, open("data/means_and_stddevs.p", "wb"))
         #Now save history in a pickle file for future use
         pickle.dump(history.history, open("history.p", "wb"))
     # Allow to keyboard interupt to not go over all epochs
