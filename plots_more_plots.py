e38835766c92fb00619205e2a66a9ee48dc32bbc
lvb5 <mcochranbranson@gmail.com>
2022-08-15 20:38:47 +0200

    thang

--- plots_more_plots.py
+++ plots_more_plots.py
@@ -35,18 +35,21 @@ if args.use_cache:
     print('Getting data from cache')
     d = np.load('data/d.npy')
 
+np.save(file='/eos/user/m/mcochran/TES_dataset/np_arrays_testing/testing_data_all', arr=d)
+
 #---------------------------------------------------------------
 # Make plots nstuff!
-from taunet.plotting import response_and_resol_vs_var
-from taunet.utils import copy_plots_to_cernbox
+# from taunet.plotting import response_and_resol_vs_var
+# from taunet.utils import copy_plots_to_cernbox
 
-response_and_resol_vs_var(d, 'perf_plots')
-copy_plots_to_cernbox(location='perf_plots')
+# response_and_resol_vs_var(d, 'perf_plots')
+# copy_plots_to_cernbox(location='perf_plots')
 
-pltText = ['1p0n', '1p1n', '1pXn', '3p0n', '3pXn']
-pltVars = ['pt', 'eta', 'mu']
-for i in range(5):
-    dtemp = np.array(d[d['TauJetsAuxDyn.NNDecayMode'] == i])
-    for j in range(len(pltVars)):
-        response_and_resol_vs_var(dtemp, 'perf_plots/DecayMode{}'.format(i), xvar=pltVars[j], nbins=15)
-        copy_plots_to_cernbox(location='perf_plots/DecayMode{}'.format(i))
+# pltText = ['1p0n', '1p1n', '1pXn', '3p0n', '3pXn']
+# pltVars = ['pt', 'eta', 'mu']
+# for i in range(5):
+#     dtemp = np.array(d[d['TauJetsAuxDyn.NNDecayMode'] == i])
+#     np.save(file='/eos/user/m/mcochran/TES_dataset/np_arrays_testing/NNDecayMode{}'.format(i), arr=dtemp)
+    # for j in range(len(pltVars)):
+    #     response_and_resol_vs_var(dtemp, 'perf_plots/DecayMode{}'.format(i), xvar=pltVars[j], nbins=15)
+    #     copy_plots_to_cernbox(location='perf_plots/DecayMode{}'.format(i))
